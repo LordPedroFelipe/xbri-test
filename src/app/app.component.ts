d87fe9ec2b36f2d027a8ff7f2a5fa73696f9697b
pedro.souza <pedro.souza@martinellitech.com.br>
2025-11-05 18:21:43 -0300

    feat: Projto inicial de teste para Xbri Frontend Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'XRI PNEUS';
+  subTitle = 'PORTAL DE GARANTIA';
+
+  titlePage = 'Solicitação de garantia';
+  subTitlePage = 'Preencha seus dados e conte com a nossa ajuda!';
+  
+
+}
